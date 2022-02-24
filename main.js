c3c17b1d827e21886e1866dcfb224a9161ba843c
Dhava2010 <87840766+Dhava2010@users.noreply.github.com>
2022-02-23 20:38:06 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,33 @@
-img = "";
 status = "";
-objects = [];
+video = "";
+
 function preload()
 {
-    img = loadImage('dog_cat.jpg');
+    video = createVideo("video.mp4");
+    video.hide();
 }
 
 function setup()
 {
     canvas = createCanvas(700, 500);
     canvas.center();
-    video = createCapture(VIDEO);
-    video.size(700, 500);
-    video.hide();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("whatsappstatus").innerHTML = "Status: detecting objects";
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Recognizing..."
 }
 
 function modelLoaded()
 {
-    console.log("Model Loaded");
     status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(1);
 }
 
-function gotResult(error, results)
-{
-    if(error)
-    {
-        console.error();
-    }
-    console.log(results);
-
-    objects = results;
-
-}
 function draw()
 {
     image(video, 0, 0, 700, 500);
-    if(status!="")
-    {
-        r = random(255);
-        g = random(255);
-        b = random(255);
-        objectDetector.detect(video, gotResult);
-        for(i = 0; i < objects.length; i++)
-        {
-            document.getElementById("whatsappstatus").innerHTML = "Status: 🅳🅴🆃🅴🅲🆃🅴🅳";
-            document.getElementById("noo").innerHTML = "Number of detected objects are" + objects.length; 
-            fill(r, g, b);
-            percent = floor(objects[i].confidence*100);
-            text(objects[i].label + " " + percent+"%", objects[i].x + 15, objects[i].y + 10);
-            noFill();
-            stroke(r, g, b);
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
-    }
 }
